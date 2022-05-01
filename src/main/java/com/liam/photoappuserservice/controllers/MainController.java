6d2783d912a94f51dda1d8eec6b138671d21a417
Liam Desmond <liamjdesmond84@gmail.com>
2022-05-01 15:23:20 -0500

    Updating application.yml with random instance_id

--- src/main/java/com/liam/photoappuserservice/controllers/MainController.java
+++ src/main/java/com/liam/photoappuserservice/controllers/MainController.java
@@ -1,8 +1,16 @@
 package com.liam.photoappuserservice.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/users")
 public class MainController {
+	
+	@GetMapping("/status/check")
+	public String status() {
+		return "Working";
+	}
 
 }
