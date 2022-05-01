af8bb91c0b74a025573abab3c771d67fbadc2c09
Liam Desmond <liamjdesmond84@gmail.com>
2022-05-01 16:41:43 -0500

    @Autowired Environment to check local.server.port(running port)

--- src/main/java/com/liam/photoappuserservice/controllers/MainController.java
+++ src/main/java/com/liam/photoappuserservice/controllers/MainController.java
@@ -1,5 +1,9 @@
 package com.liam.photoappuserservice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class MainController {
 	
+	@Value("${spring.application.name}")
+	private String appName;
+
+	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/status/check")
 	public String status() {
-		return "Working";
+		return "Working on port " + env.getProperty("local.server.port") + " & App name is: " + appName;
 	}
 
 }
