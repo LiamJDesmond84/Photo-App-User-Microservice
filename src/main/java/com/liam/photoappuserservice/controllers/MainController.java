9786b2ea55496e99eb5ff639c91c89107cf73f22
Liam Desmond <liamjdesmond84@gmail.com>
2022-05-01 19:40:36 -0500

    Adding User model

--- src/main/java/com/liam/photoappuserservice/controllers/MainController.java
+++ src/main/java/com/liam/photoappuserservice/controllers/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,14 @@ public class MainController {
 	@GetMapping("/status/check")
 	public String status() {
 		return "Working on port " + env.getProperty("local.server.port") + " & App name is: " + appName;
+	}
+	
+	
+	
+	@PostMapping
+	public String createUser() {
+		
+		return "Create User method";
 	}
 
 }
---
+++ src/main/java/com/liam/photoappuserservice/models/User.java
@@ -0,0 +1,69 @@
+package com.liam.photoappuserservice.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	
+	@NotBlank
+	String firstName;
+	
+	@NotBlank
+	String lastName;
+	
+	@Email
+	String email;
+
+	public User() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	
+	
+	
+	
+
+}
