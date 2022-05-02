f1ba1def0d9ac046484fff8123f5589e16a79cd4
Liam Desmond <liamjdesmond84@gmail.com>
2022-05-02 18:39:47 -0500

    MapStruct working...  I think...

---
+++ src/main/java/com/liam/photoappuserservice/services/UserMapper.java
@@ -0,0 +1,23 @@
+package com.liam.photoappuserservice.services;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.liam.photoappuserservice.models.User;
+import com.liam.photoappuserservice.shared.UserDTO;
+
+@Mapper
+public interface UserMapper {
+	
+	UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+	
+	
+//	User sourceToDestination(UserDTO userDTO);
+//	UserDTO destinationToSource(User user);
+	
+	UserDTO sourceToDestination(User user);
+	User destinationToSource(UserDTO userDTO);
+//	UserDTO tDto(User user);
+
+}
--- src/main/java/com/liam/photoappuserservice/services/UserServiceImpl.java
+++ src/main/java/com/liam/photoappuserservice/services/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.liam.photoappuserservice.services;
 
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.liam.photoappuserservice.models.User;
 import com.liam.photoappuserservice.repositories.UserRepository;
 import com.liam.photoappuserservice.shared.UserDTO;
 
@@ -16,7 +19,17 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public UserDTO createUser(UserDTO newUser) {
-		// TODO Auto-generated method stub
+		User user = new User();
+		user.setFirstName(newUser.getFirstName());
+		user.setLastName(newUser.getLastName());
+		user.setEmail(newUser.getEmail());
+		user.setPassword(newUser.getPassword());
+		
+//		UserDTO userDTO = UserMapper.INSTANCE.tDto(null);
+
+		System.out.println(ReflectionToStringBuilder.toString(user));
+		System.out.println(ReflectionToStringBuilder.toString(newUser));
+
 		return userRepo.save(newUser);
 	}
 
