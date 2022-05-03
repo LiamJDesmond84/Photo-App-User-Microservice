a95d48745bbf14889fc6402c561aacd8cc5dd38f
Liam Desmond <liamjdesmond84@gmail.com>
2022-05-03 18:12:17 -0500

    Added API-Gateway IP to access only list

--- src/main/java/com/liam/photoappuserservice/security/WebSecurity.java
+++ src/main/java/com/liam/photoappuserservice/security/WebSecurity.java
@@ -1,6 +1,8 @@
 package com.liam.photoappuserservice.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,11 +12,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private Environment env;
+	
 	@Override
 	protected void configure(HttpSecurity httpSec) throws Exception {
 		
+		
 		httpSec.csrf().disable();
-		httpSec.authorizeHttpRequests().antMatchers("/api/users/**").permitAll();
+//		httpSec.authorizeHttpRequests().antMatchers("/api/users/**").permitAll();
+//		httpSec.authorizeRequests().antMatchers("/api/users/**").permitAll();
+		
+		// Only allows requests from certain IP addresses(API-Gatway in this example)
+		httpSec.authorizeRequests().antMatchers("/**").hasIpAddress(env.getProperty("gateway.ip"));
+		
+		// Allows H-2 Console.
+		httpSec.headers().frameOptions().disable();
 		
 	}
 
